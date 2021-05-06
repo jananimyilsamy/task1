4d4b43cd8e42a7f73cd42d8fd265e6eeff0ccd6e
jananimyilsamy <49332397+jananimyilsamy@users.noreply.github.com>
2021-05-06 18:29:25 +0530

    Add files via upload

---
+++ github task2/src/App.js
@@ -0,0 +1,16 @@
+import Motorinsurancetypes from './Components/Motorinsurancetypes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AdditionalCoverage from './Components/AdditionalCoverage';
+
+function App () {
+  return (
+    <div className="App">
+      <AdditionalCoverage />;
+
+      <Motorinsurancetypes />
+
+    </div>
+  );
+}
+
+export default App;
